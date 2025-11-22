9abdb5e71a6d85e0e8b1b01fd713ce6fb8eb7b74
Robertson Mondragon Giron <mondragongironrobertson@gmail.com>
2025-11-22 00:42:13 -0500

    creacion de base de datos

--- sistemadegestiondecentroveterinario/src/main/java/com/sistemadegestiondecentroveterinario/sistemadegestiondecentroveterinario/model/Propietario.java
+++ sistemadegestiondecentroveterinario/src/main/java/com/sistemadegestiondecentroveterinario/sistemadegestiondecentroveterinario/model/Propietario.java
@@ -1,7 +1,7 @@
 package com.sistemadegestiondecentroveterinario.sistemadegestiondecentroveterinario.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import java.util.ArrayList;
 import java.util.List;
 
